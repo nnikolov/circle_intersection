c2f22c40906846eb54ff7041e381e8586270f7bc
Nick Nikolov <nrnickolov@yahoo.com>
2018-08-28 15:05:06 -0400

    Check if circles intersect

--- lib/circle_intersection/circles.rb
+++ lib/circle_intersection/circles.rb
@@ -12,4 +12,29 @@ class Circles
   def distance_between_circle_centers
     Math.sqrt((circle2.x - circle1.x) ** 2 + (circle2.y - circle1.y) ** 2)
   end
+
+  def intersection?
+    return false if separate_circles?
+    return false if contained_within?
+    return false if coincident?
+    true
+  end
+
+  # If the distance between the two circles is grater than the two radii, the circles are separate.
+  def separate_circles?
+    distance_between_circle_centers > circle1.r + circle2.r
+  end
+
+  # If the distance between the two circles is smaller than absolute value of radius 1 minus radius 2, 
+  # then one circle is contained within the other
+  def contained_within?
+    distance_between_circle_centers < (circle1.r - circle2.r).abs
+  end
+
+  # If the distance between the two circles is zero and radius 1 equals radius 2, then the two circles
+  # are coincident
+  def coincident?
+    distance_between_circle_centers == 0 and circle1.r == circle2.r
+  end
+
 end
--- test/circle_intersection_test.rb
+++ test/circle_intersection_test.rb
@@ -12,4 +12,24 @@ class CircleIntersectionTest < ActiveSupport::TestCase
     circles = Circles.new(Circle.new(x: 0, y: 0, r: 1), Circle.new(x: 6, y: 8, r: 2))
     assert_equal 10, circles.distance_between_circle_centers
   end
+
+  test "Testing separate circles" do
+    circles = Circles.new(Circle.new(x: 1, y: 1, r: 1), Circle.new(x: 5, y: 5, r: 1))
+    assert circles.separate_circles?
+  end
+
+  test "Testing circle within circle" do
+    circles = Circles.new(Circle.new(x: 5, y: 5, r: 15), Circle.new(x: 5, y: 5, r: 10))
+    assert circles.contained_within?
+
+    circles = Circles.new(Circle.new(x: 5, y: 5, r: 10), Circle.new(x: 5, y: 5, r: 15))
+    assert circles.contained_within?
+  end
+
+  test "Testing coincident circles" do
+    circle = Circle.new(x: 15, y: 15, r: 5)
+    circles = Circles.new(circle, circle)
+    assert circles.coincident?
+  end
+
 end
